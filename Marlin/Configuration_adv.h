120ff6a6a4f6a0ed7d288863f5e2711841d6915b
Ruben Ollesch <ruben.ollesch@protonmail.com>
2026-03-05 16:36:31 +0100

    Drastically reduce Z axis homing current
    This should reduce any possible collision damage, while still working
    fine due to the lead screw's torque multiplication.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2772,7 +2772,7 @@
 
   #if AXIS_IS_TMC_CONFIG(Z)
     #define Z_CURRENT       800
-    #define Z_CURRENT_HOME  Z_CURRENT
+    #define Z_CURRENT_HOME  100
     #define Z_MICROSTEPS     16
     #define Z_RSENSE          0.11
     #define Z_CHAIN_POS      -1
